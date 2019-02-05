db0f7779ecd200f267c0692195acb518b1b170b2
Mai Quang Nhat <quangnhat224@gmail.com>
2019-02-05 17:29:51 +0200

    adding column obstacles

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -5,22 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class GameControl : MonoBehaviour
 {
-    public static GameControl instance;
-    public GameObject gameOvertext;
-    public bool gameOver = false;
+    public static GameControl instance;         //A reference to our game control script so we can access it statically.
+    public Text scoreText;                      //A reference to the UI text component that displays the player's score.
+    public GameObject gameOvertext;             //A reference to the object that displays the text which appears when the player dies.
+
+    private int score = 0;                      //The player's score.
+    public bool gameOver = false;               //Is the game over?
     public float scrollSpeed = -1.5f;
+
 
     void Awake()
     {
-       if (instance == null) {
-         instance = this;
-       }
-       else if (instance != this) {
-         Destroy(gameObject);
-       }
-
+        //If we don't currently have a game control...
+        if (instance == null)
+            //...set this one to be it...
+            instance = this;
+        //...otherwise...
+        else if(instance != this)
+            //...destroy this one because it is a duplicate.
+            Destroy (gameObject);
     }
-
 
     void Update()
     {
@@ -31,6 +35,18 @@ public class GameControl : MonoBehaviour
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
+
+    public void BirdScored()
+    {
+        //The bird can't score if the game is over.
+        if (gameOver)
+            return;
+        //If the game is not over, increase the score...
+        score++;
+        //...and adjust the score text.
+        scoreText.text = "Score: " + score.ToString();
+    }
+
     public void BirdDied()
     {
         //Activate the game over text.
